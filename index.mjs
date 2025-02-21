01b9e10b35e61769c51de5d708f5be2cb08a495c
Oleksandr Garmen <148455522+OleksandrGarmen@users.noreply.github.com>
2025-02-21 15:54:50 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,15 +4,15 @@ const data = JSON.parse(jsonData); console.log(data);
 
 
 //TASK #1 | LEVEL #1  | MAP
-const productnames = data.map(product => product.name);
+const productnames =  data.map(product => product.name);
 console.log(productnames);
 
 
 
 
 //TASK #2 | LEVEL #1 | MAP
-const pricedeta = data.map(product => ({
-  name: product.name,
+const pricedeta =  data.map(product => ({
+   name: product.name,
   price: product.price
 }));
 console.log(pricedeta)
@@ -22,7 +22,7 @@ console.log(pricedeta)
 
 //TASK #3 | LEVEL #1 | FILTER
 const products = data.map(product => ({
-  name: product.name,
+   name: product.name,
   inStock: product.inStock
 }));
 console.log(products);
@@ -48,36 +48,36 @@ const electronics = data.filter(product => product.category === "Electronics").m
 
 
 //TASK #6 | LEVEL #1 | FILTER
-const rating = data.filter(product => product.rating >=4.5).map(product => ({
+const rating = data.filter(product =>  product.rating >=4.5).map(product => ({
     name: product.name,
-    rating: product.rating
+  rating: product.rating
   }));
-console.log(rating);
+console.log( rating);
 
 
 //TASK #6 | LEVEL #1 | REDUCE
-const total = data.reduce((accumulator, product) => accumulator  + product.price, 0);
+const total = data.reduce((accumulator,  product) => accumulator  + product.price, 0);
 console.log(total);
 
 
 
 //TASK #7 | LEVEL #1 | REDUCE
-const average = data.reduce((accumulator, product) => accumulator + product.price, 0) / data.length;
+const average = data.reduce((accumulator,  product) => accumulator + product.price, 0) / data.length;
 console.log(average);
 
 
 
 
 //TASK #9 | LEVEL #2 
-const expensive = data.reduce((max, product) => product.price > max.price ? product : max);
+const expensive = data.reduce(( max, product) => product.price > max.price ? product : max);
 const cheap = data.reduce((min, product) => product.price < min.price ? product : min);
 console.log([ { name: cheap.name, price: cheap.price } ]);
 console.log([ { name: expensive.name, price: expensive.price } ]);
 
 //TASK #10 | LEVEL #2 
-const average = data.reduce((acc, product) => acc + product.rating, 0) / data.length;
+const average = data.reduce(( acc, product) => acc + product.rating, 0) / data.length;
 console.log(average);
 
 //TASK #12 | LEVEL #2 
-const quantities = data.map(product => product.reviews.length).reduce((acc, reviewsCount) => acc + reviewsCount, 0);
+const quantities = data.map( product => product.reviews.length).reduce((acc, reviewsCount) => acc + reviewsCount, 0);
 console.log(quantities);
